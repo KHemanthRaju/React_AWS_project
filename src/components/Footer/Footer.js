d550b2d4f61d5622bf427af4a3d18c60b06a7025
Koneti Hemanth Raju <rajuhemanth456@gmail.com>
2024-02-04 09:24:07 +0530

    adding content

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,38 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <>
+      <footer>
+        <div className="container">
+          <div className="row">
+            <div className="footer-col">
+              <h4>Resources for</h4>
+              <ul>
+                <li>Careers</li>
+                <li>Developer</li>
+                <li>Investors</li>
+                <li>Partners</li>
+                <li>Startups</li>
+                <li>Students and Educators</li>
+              </ul>
+            </div>
+            <div className="footer-col">
+              <h4>Why Oracle</h4>
+              <ul>
+                <li>Analyst Reports</li>
+                <li>Cloud Economics</li>
+                <li>with Microsoft Azure</li>
+                <li>vs. AWS</li>
+                <li>vs. Google Cloud</li>
+                <li>vs. MongoDB</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
 };
 
 export default Footer;
